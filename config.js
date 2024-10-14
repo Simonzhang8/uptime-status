5feab24495b20326d5b7485047d38d7766b15293
张先淼 <165749751+Simonzhang8@users.noreply.github.com>
2024-10-14 17:27:35 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,33 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '张先淼的个人网站',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur2560714-7244e0ec713da5532dbc7f22',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '个人网站',
+      url: 'https://www.zxm86.cn/'
+    },
+    {
+      text: '网址导航',
+      url: 'https://portal.zxm86.cn'
+    },
+    {
+      text: '联系站长',
+      url: 'mailto:contact@zxm86.cn'
+    },
+  ],
+};
